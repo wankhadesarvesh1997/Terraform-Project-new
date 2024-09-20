53dc6babbcaedec126a80edd160ded05d868f40e
ponnapalli1997 <leeminhoakhi@gmail.com>
2024-09-20 23:00:29 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,6 +1,6 @@
 resource "aws_elb" "bar" {
-  name               = "raham-terraform-elb"
-  availability_zones = ["ap-southeast-1a", "ap-southeast-1b"]
+  name               = "akhi-terraform-elb"
+  availability_zones = ["us-west-1a", "us-west-1b"]
 
   listener {
     instance_port     = 80
@@ -21,7 +21,7 @@ resource "aws_elb" "bar" {
   cross_zone_load_balancing = true
   idle_timeout              = 400
   tags = {
-    Name = "raham-tf-elb"
+    Name = "akhila-tf-elb"
   }
 }
 
